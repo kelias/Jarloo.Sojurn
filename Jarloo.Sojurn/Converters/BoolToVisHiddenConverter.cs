3a0d8fc97c055fab3adc50277b20db7107879719
Kelly Elias <kelly.elias@gmail.com>
2017-03-10 12:27:04 -0700

    fixes

---
+++ Jarloo.Sojurn/Converters/BoolToVisHiddenConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Jarloo.Sojurn.Converters
+{
+    public class BoolToVisHiddenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return (bool)value ? Visibility.Visible : Visibility.Hidden;
+            }
+            catch
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jarloo.Sojurn/Converters/NumberExtensionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Jarloo.Sojurn.Converters
+{
+    public class NumberExtensionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var num = (int) value;
+
+            if (num == 1) return $"{num}st";
+            if (num ==2) return $"{num}nd";
+            if (num ==3) return $"{num}rd";
+            return $"{num}th";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
