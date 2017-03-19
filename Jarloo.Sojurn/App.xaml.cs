cd65c1db230e618223a53756588141b968a075b4
Kelly Elias <kelly.elias@gmail.com>
2017-03-19 00:53:11 -0600

    removed comments

--- Jarloo.Sojurn/App.xaml.cs
+++ Jarloo.Sojurn/App.xaml.cs
@@ -12,15 +12,6 @@ namespace Jarloo.Sojurn
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            //List<TvStream> streams = new List<TvStream>();
-
-            //streams.Add(new TvStream("ProjectFreeTv", "http://project-free-tv.im/episode/{0}-season-{1}-episode-{1}/",TvStream.SeparationCharacter.Underscore));
-
-            //var json = streams.ToJson();
-
-            //File.WriteAllText("e:\\saved.json",json);
-
-
             ViewModelManager.Create<MainViewModel>().Show();
         }
     }
