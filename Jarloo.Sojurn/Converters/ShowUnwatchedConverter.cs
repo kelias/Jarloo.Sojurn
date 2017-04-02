4de57870393621474392af3cd450cca3110fe59f
Kelly Elias <kelly.elias@gmail.com>
2017-04-01 22:12:45 -0600

    Added a visual indicator on shows to correlate the backlog, and ordered shows by the number of unwatched shows.

---
+++ Jarloo.Sojurn/Converters/ShowUnwatchedConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Jarloo.Sojurn.Converters
+{
+    public class ShowUnwatchedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var i = (int) value;
+                return i > 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+            catch
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
